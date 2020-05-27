0a7b165e46345698c7a8c9e41aad3a6defef3b2c
Goy288 <44177200+Goy288@users.noreply.github.com>
2020-05-26 23:43:59 -0700

    lmao doing this in all one commit

--- BusinessLogic/Course.cs
+++ BusinessLogic/Course.cs
@@ -6,9 +6,12 @@ namespace BusinessLogic
 {
     public class Course
     {
+        private byte numberOfCredits;
+
         public Course(string courseName)
         {
             CourseName = courseName;
+            InstructorName = "STAFF";
         }
 
         /// <summary>
@@ -19,12 +22,31 @@ namespace BusinessLogic
         /// <summary>
         /// The name of the course
         /// </summary>
-        public string CourseName { get; set; }
+        public string CourseName { 
+            get => CourseName; 
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+        }
 
         /// <summary>
         /// Number of credits for the course
         /// </summary>
-        public byte NumberOfCredits { get; set; }
-
+        public byte NumberOfCredits
+        {
+            get => numberOfCredits;
+            set
+            {
+                if (value == 0 || value > 30)
+                {
+                    throw new ArgumentException();
+                }
+                numberOfCredits = value; 
+            } 
+        }
     }
 }
